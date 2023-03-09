8427b9bb19b88342365168a4fd356a77fa2c8034
raymondborel <rborel852@gmail.com>
2023-03-09 13:53:25 -0800

    Added Random Card Assignment

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
 let squares = document.querySelectorAll('.square');
-// console.log(squares)
-// console.log(squares[0]);
 let startGameButton = document.querySelector('.newGame')
-// console.log(startGameButton)
-let random = [1,1,2,2,3,3,4,4];
-let randomCopy = random
+//First, initializing 2 arrays
+let numArray = [];
+let copyNumArray = [];
 
-//
-// console.log(random[7])
+//Creating array of random number for half cards that doesnt repeat
+let num = Math.floor(Math.random() * 4)
+for (let i = 0; i<4; i++){
+    while (numArray.includes(num)) {
+        num = Math.floor(Math.random() * 4);
+    }
+    numArray.push(num);
+}
+//Creating second array for other half of cards
+num = Math.floor(Math.random() * 4)
+for (let i = 0; i<4; i++){
+    while (copyNumArray.includes(num)) {
+        num = Math.floor(Math.random() * 4);
+    }
+    copyNumArray.push(num);
+}
+console.log(numArray);
+console.log(copyNumArray);
+
+//Combining both arrays in 1 to set up next function which gives crads numbers
+let cardNums = numArray.concat(copyNumArray);
+console.log(cardNums);
+
 
 //Setting values for each card at newGame
-function newGame () {
+function newGame (numbers) {
+    console.log(numbers)
     for (let i = 0; i<squares.length; i++){
-        squares[i]=random[i];
-        Math.floor(Math.random() * 10);
+        //Picking a number from random to add to card
+        // let pickedNum = Math.floor(Math.random() * squares.length);
+        // console.log(numbers[i])
+        squares[i]=numbers[i];
+        // Math.floor(Math.random() * squares.length);
         squares[i].addEventListener('click', function () {
             // console.log(random[i]);
-            squares[i].innerHTML = random[i]
+            squares[i].innerHTML = numbers[i];
         })
     }
 }
-console.log(newGame);
+console.log(newGame(cardNums));
 //unfinished
 function showCard (clickedCard){
     clickedCard.innerHTML = squares
@@ -37,4 +60,4 @@ function showCard (clickedCard){
 // selectACard.addEventListener('click', function () {
 
 // })
-startGameButton.addEventListener('click', newGame);//
+// startGameButton.addEventListener('click', newGame);
