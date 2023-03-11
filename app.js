03b896b4a0d912796db46c8d3ed3b08e603779c8
raymondborel <rborel852@gmail.com>
2023-03-11 10:27:45 -0800

    FINISHED

--- app.js
+++ app.js
@@ -21,14 +21,7 @@ let square6 = squares[5];
 let square7 = squares[6];
 let square8 = squares[7];
 let cardNums;
-//First, initializing 2 arrays
 
-//Creating array of random number for half cards that doesnt repeat
-
-//Combining both arrays in 1 to set up next function which gives cards numbers
-
-
-//Setting logic fr newGame
 startGameButton.addEventListener('click', newGame);
 function newGame () {
     for (let i = 0; i<4; i++){
@@ -37,7 +30,6 @@ function newGame () {
         }
         numArray.push(num);
     }
-    //Creating second array for other half of cards
     num = Math.floor(Math.random() * 4)
     for (let i = 0; i<4; i++){
         while (copyNumArray.includes(num)) {
@@ -46,7 +38,6 @@ function newGame () {
         copyNumArray.push(num);
     }
     cardNums = numArray.concat(copyNumArray);
-    //Setting vales & event listeners for each card
     for (let i = 0; i<squares.length; i++){
         squares[i].addEventListener('click', function ()  {
             if(!isFirstCardSelected) {
@@ -59,46 +50,10 @@ function newGame () {
         })
     }
     
-    console.log(cardNums)
+
 }
-// startGameButton.addEventListener('click', newGame);
-
-
-
-
-//  function checkMatch (firstGuessCard) {
-//      for (let i=0; i<squares.length; i++){
-//         squares[i].addEventListener('click', function () {
-//             for (let i=0; i<squares.length; i++){
-//                 for (let j=0; j<squares.length; j++) {  
-//                     if (squares[i].innerHTML !== squares[j].innerHTML) {
-//                          squares[j].innerHTML = ''         
-//                          squares[i].innerHTML = ''
-//                     }
-//                 }
-//             }
-//         })
-//      }   
-//  }
-// console.log(newGame(cardNums));
-// Working on hide and reveal logic
-
-//  squares[i].addEventListener('click', function () {
-//      squares[i].innerHTML = numbers[i];
-//  })
-
-
-
-//unfinished
-// function hide(whichSquare) {
-//     document.getElementById('whichSquare').style.display = 'none'
-//  }
-
-
-//   hide(square1);
 function showFirstCard (square, squareValue) {
     if ( isFirstCardSelected == false){
-        //isFirstCardSelected = true;
         square.innerHTML = squareValue;
     }
 }
@@ -111,15 +66,11 @@ function showSecondCard (square, idx) {
         firstPickedCard = false;
         isFirstCardSelected = false;
     } else { 
-        //show cardnum [idx]
         firstPickedCard.innerHTML = ''
         firstPickedCard = false;
         isFirstCardSelected = false;
     }
 }
-
-// }
-// Quit game function clears board
 quitGameButton.addEventListener('click', quitGame)
 function quitGame (){
         for ( i=0; i<squares.length; i++){
@@ -128,26 +79,3 @@ function quitGame (){
     matches = 0
     numOfMatches.innerHTML = `Correct Matches ${matches}`  
 }
-//trying to set event listener for each card so I can show card after click
-// for (let i = 0; i < squares.length; i++){
-
-// }
-
-// function startTimer(seconds) {
-//     myTimer = seconds;
-//     const interval = setInterval( function () {
-//         clock.innerHTML = (`$(myTimer)`)
-//         myTimer--;
-//         if (myTimer <0) {
-//             clearInterval(interval);
-//             clock.innerHTML = ('Time is up!! GAME OVER!');
-//         }
-//     }, 1000);
-// }
-
-
-//unfinished
-// selectACard.addEventListener('click', function () {
-
-// })
-// startGameButton.addEventListener('click', newGame);
